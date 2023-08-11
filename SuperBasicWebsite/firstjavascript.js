f6dd23fa416d7f8191d82a30f7b6c749386bf146
Shaheer Khan <119394707+shaheerkhan8476@users.noreply.github.com>
2023-08-11 09:14:28 -0400

    Add files via upload

---
+++ SuperBasicWebsite/firstjavascript.js
@@ -0,0 +1,4 @@
+function showInput() {
+    var name = document.getElementById('name').value;
+    document.getElementById('outputName').innerHTML = 'Thank You'+'&nbsp;' + name + '. You will be contacted soon.';
+}
